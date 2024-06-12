526bcbf8a7ea4e1b4a19fb39c5989405389ea39d
mads stenkær <mns@sensade.com>
2024-06-12 10:35:41 +0200

    Updated adr with corrected return value

--- configuration/chirpstack/adr-modules/sensade-adr-mod/sensade-adr.js
+++ configuration/chirpstack/adr-modules/sensade-adr-mod/sensade-adr.js
@@ -12,7 +12,7 @@ export function id() {
 //
 // Input object example:
 // {
-//  regionName: "eu868",
+//  regionConfigId: "eu868",
 //  regionCommonName: "EU868",
 //  devEui: "0102030405060708",
 //  macVersion: "1.0.3",
@@ -43,114 +43,80 @@ export function id() {
 //  txPowerIndex: 1,
 //  nbTrans: 1
 // }
-// Handle function for ADR request.
 export function handle(req) {
-  // This defines the default response, which is equal to the current device state.
-  let resp = {
-    dr: req.DR,
-    txPowerIndex: req.TxPowerIndex,
-    nbTrans: req.NbTrans,
-  };
+  var resp = req;
 
   resp.txPowerIndex = 0;
 
-  // If ADR is disabled, return with current values.
-  if (!req.ADR) {
-    return [resp, null];
+  if (!req.adr) {
+    return {
+      dr: resp.dr,
+      txPowerIndex: resp.txPowerIndex,
+      nbTrans: resp.nbTrans
+    };
   }
 
-  let lostPackageCnt = getLostPackageCount(req);
-  // If 5 or more packages are lost during the last received packages, decrease the data-rate
-  if (lostPackageCnt >= 5) {
+  var lostPackageCntNew = getLostPackageCount(req.uplinkHistory, 20);
+
+  if (lostPackageCntNew >= 5) {
     resp.dr -= 1;
-  } else if (
-    req.UplinkHistory.length === 20 &&
-    getLostPackageCount(req, 20) <= 2
-  ) {
-    // If 2 or fewer packages are lost during the last 20 received packages, the dr might be able to be increased
+  } else if (req.uplinkHistory.length >= 20 && getLostPackageCount(req.uplinkHistory, 20) <= 2) {
+    var minSnr = getMinSnr(req.uplinkHistory);
+    var installationMargin = 10;
 
-    // We only want to increase the dr if none of the previously received packages
-    // are within the installationMargin of the minimum required snr for the current dr
-    // The installationMargin should prevent us from increasing the dr too much, such that packages are lost
-    // when cars park in a spot.
-    let minSNR = getMinSNR(req);
-    let installationMargin = 10;
-    // A margin of around 10 seems okay
-    // This value has been chosen based on the loRaSNR values from sensors installed in a parking lot,
-    // where a fluctuation of +- 10 can be seen (should be caused by cars parking and leaving).
-    let diffSNR = minSNR - req.requiredHistoryCount - installationMargin;
+    var diffSnr = minSnr - req.requiredSnrForDr - installationMargin;
 
-    // Examples:
-    // minSNR = -5
-    // requiredSNRforDR = -10
-    // installationMargin = 10
-    // diffSNR = -5 - -10 - 10 = -5
-    // => Not safe to increase the dr
-
-    // minSNR = 5
-    // requiredSNRforDR = -10
-    // installationMargin = 10
-    // diffSNR = 5 - -10 - 10 = 5
-    // => The dr should be safe to increase
-
-    if (diffSNR > 0) {
+    if (diffSnr > 0) {
       resp.dr += 1;
     }
   }
 
-  // If the data rate is increased or reduced to something outside the Min - Max range,
-  // set it to the respective limit
   if (req.maxDr < resp.dr) {
     resp.dr = req.maxDr;
   }
 
-  if (req.MinDR > resp.dr) {
-    resp.dr = req.MinDR;
+  if (req.minDr > resp.dr) {
+    resp.dr = req.minDr;
   }
 
-  return [resp, null];
+  return {
+    dr: resp.dr,
+    txPowerIndex: resp.txPowerIndex,
+    nbTrans: resp.nbTrans
+  };
 }
 
-function getMinSNR(req) {
-  let snrM = 999;
 
-  for (const uh of req.uplinkHistory) {
-    if (uh.maxSnr < snrM) {
-      snrM = uh.maxSnr;
+function getLostPackageCount(uplinkHistory, lastXElements) {
+  var len = uplinkHistory.length;
+
+  if (len < lastXElements) {
+    lastXElements = len;
+  }
+
+  var elements = uplinkHistory.slice(-lastXElements);
+
+  var prevFcnt = elements[0].fCnt;
+  var lostPackages = 0;
+
+  for (var element of elements.slice(1)) {
+    var curFcnt = element.fCnt;
+    var diff = curFcnt - prevFcnt;
+
+    lostPackages += diff - 1;
+    prevFcnt = curFcnt;
+  }
+
+  return lostPackages;
+}
+
+function getMinSnr(uplinkHistory) {
+  var snrM = 999;
+
+  for (var element of uplinkHistory) {
+    if (element.maxSnr < snrM) {
+      snrM = element.maxSnr;
     }
   }
   return snrM;
-}
-
-// Function to get the count of lost packages from the request.
-function getLostPackageCount(req, ...lastXElement) {
-  if (req.uplinkHistory.length < 2) {
-    return 0;
-  }
-
-  let elements = req.uplinkHistory;
-
-  if (lastXElement.length > 0) {
-    let x = lastXElement[0];
-    if (x < elements.length) {
-      elements = elements.slice(elements.length - x);
-    }
-  }
-
-  let lostPackets = 0;
-  let previousFCnt = 0;
-
-  for (let i = 0; i < elements.length; i++) {
-    const m = elements[i];
-
-    if (i === 0) {
-      previousFCnt = m.FCnt;
-      continue;
-    }
-
-    lostPackets += m.FCnt - previousFCnt - 1; // there is always an expected difference of 1
-    previousFCnt = m.FCnt;
-  }
-
-  return lostPackets;
 }
